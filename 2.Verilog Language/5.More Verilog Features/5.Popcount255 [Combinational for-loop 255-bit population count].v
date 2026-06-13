fdcbffbd2f5373c6539fc9cc393bfbdb28d99d0f
Abdulbaasit Erinkitola <eabdulbaasit@gmail.com>
2026-06-14 00:23:51 +0100

    feat: practice more verilog features

---
+++ 2.Verilog Language/5.More Verilog Features/5.Popcount255 [Combinational for-loop 255-bit population count].v
@@ -0,0 +1,10 @@
+module top_module( 
+    input [254:0] in,
+    output reg [7:0] out );
+    always @* begin
+        out = 8'd0;
+        for (int i = 0; i<$bits(in); i = i+1)
+            out = in[i] == 1 ? out+1 : out;
+    end
+
+endmodule
